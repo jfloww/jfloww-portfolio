4e04537c72c5c1ea5e625b8770d7e829ec859f3f
jfloww <hoon7589@gmail.com>
2025-01-31 09:15:12 -0500

    deploy test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/jfloww.github.io",
 };
 
 export default nextConfig;
