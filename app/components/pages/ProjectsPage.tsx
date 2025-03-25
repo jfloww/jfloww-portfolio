12ef22533b72f7f46329af7c060dbd9e1d2ce775
jfloww <hoon7589@gmail.com>
2025-03-25 10:00:30 -0500

    feat: Refactorization in directory structure

---
+++ app/components/pages/ProjectsPage.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { getPostList } from '../functions/importList';
+
+export default async function ProjectsPage() {
+  const postList = await getPostList('projects', 5);
+  return (
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {postList.map((post) => (
+          <Link href={`/projects/${post.id}`} key={post.id}>
+            <div className="bg-white dark:bg-black border border-gray-200 dark:border-gray-700 rounded-lg shadow hover:shadow-xl transition-transform transform hover:scale-105 cursor-pointer">
+              <div className="relative w-full h-48 rounded-t-lg overflow-hidden rounded-lg bg-black">
+                <Image src={post.images?.[0].src ?? '/default-image.jpg'} alt="proj image" fill className="m-auto p-4" />
+              </div>
+              <div className="p-4 flex flex-col w-full">
+                <div className="flex w-full justify-between">
+                  <h2 className="text-lg font-semibold text-gray-900 dark:text-gray-100">{post.title}</h2>
+                  <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
+                    {`${post.date.substring(0, 4)}-${post.date.substring(4, 6)}-${post.date.substring(6, 8)}`}
+                  </p>
+                </div>
+                <div>{post.techStack}</div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/ko/page.tsx
+++ app/ko/page.tsx
@@ -1,23 +1,18 @@
-"use client";
-import Link from "next/link";
+'use client';
+import Link from 'next/link';
 
-export default function HomeKO() {
+export default function KoHomePage() {
   return (
     <div className="p-6 w-full justify-center">
       <section className="flex flex-col items-center text-center mt-16 w-full">
         <h1 className="text-5xl font-bold w-full">안녕하세요</h1>
         <h1 className="text-5xl font-bold w-full">Welcome to my Page</h1>
         <h2 className="">Software Engineer</h2>
-        <Link
-          href="/about"
-          className="mt-6 bg-blue-500 px-6 py-3 rounded-lg shadow-md hover:bg-blue-600"
-        >
+        <Link href="/about" className="mt-6 bg-blue-500 px-6 py-3 rounded-lg shadow-md hover:bg-blue-600">
           더보기
         </Link>
       </section>
-      <section className="text-5xl m-auto text-center mt-20">
-        Comming Soon!
-      </section>
+      <section className="text-5xl m-auto text-center mt-20">Comming Soon!</section>
       {/* <section className="w-full flex mt-24">
         <section className="w-1/2 justify-center text-center">
           <h1 className="text-3xl font-bold">Projects</h1>
